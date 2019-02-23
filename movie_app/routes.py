71a6fa06ac1ebeb2a7d174d8ec5a413c6b0a2c86
Nishad94 <nishad.dawkhar94@gmail.com>
2019-02-23 00:26:48 -0500

    Added anomaly features and multi-context dialogs

--- movie_app/routes.py
+++ movie_app/routes.py
@@ -4,102 +4,93 @@ import dialogflow
 import pprint
 import requests
 import json
-import mysql.connector
-import tmdb
+from movie_app import tmdb
+import numpy as np
+import json
 from movie_app import app
 
-OMDB_API_KEY="e5c107d2"
+
 DIALOGFLOW_PROJECT_ID="helloworld-597d8"
 GOOGLE_APP_CREDENTIALS="HelloWorld-bb6a78d1c91d.json"
 
-def getAverageRating(titles_list):
-	print "Geting avg: "
-	total = 0
-	count = 0
-	for t in titles_list:
-		if t["rating"] is not None:
-			total += t["rating"]
-			count += 1
-	return total/float(count)
+# def extract_actor_anomaly(imdb_title_id):
+# 	cnx = mysql.connector.connect(user='root', host='127.0.0.1',database='movies')
+# 	cursor = cnx.cursor()
+# 	actors_stmt = "Select actors from movie_info where tconst = '{}'".format(imdb_title_id)
+# 	print "trying query for " + imdb_title_id
+# 	try:
+# 		cursor.execute(actors_stmt)
+# 		print "checking actors"
+# 		actors = []
+# 		for actor_id in cursor:
+# 			actors.append(actor_id)
+# 		print actors
+# 		if len(actors) > 0:
+# 			actors = actors[0][0].split(",")
+# 		print "Actors in {} = {}".format(imdb_title_id, actors)
+# 		actor_titles_stmt = "select titles,name from name_info where nconst='{}'"
+# 		actor_title_map = {}
+# 		actor_name_map = {}
+# 		for actor in actors:
+# 			cursor.execute(actor_titles_stmt.format(actor))
+# 			titles = []
+# 			names = []
+# 			for t in cursor:
+# 				print t
+# 				print "heyyyyyy"
+# 				titles.append(t[0])
+# 				names.append(t[1])
+# 			titles = titles[0].split(",")
+# 			actor_title_map[actor] = titles
+# 			names = names[0]
+# 			actor_name_map[actor] = names
+# 		print "Actor and Titles: \n" + str(actor_title_map)
+# 		print "Actor and Name: \n" + str(actor_name_map)
 
-def extract_actor_anomaly(imdb_title_id):
-	cnx = mysql.connector.connect(user='root', host='127.0.0.1',database='movies')
-	cursor = cnx.cursor()
-	actors_stmt = "Select actors from movie_info where tconst = '{}'".format(imdb_title_id)
-	print "trying query for " + imdb_title_id
-	try:
-		cursor.execute(actors_stmt)
-		print "checking actors"
-		actors = []
-		for actor_id in cursor:
-			actors.append(actor_id)
-		print actors
-		if len(actors) > 0:
-			actors = actors[0][0].split(",")
-		print "Actors in {} = {}".format(imdb_title_id, actors)
-		actor_titles_stmt = "select titles,name from name_info where nconst='{}'"
-		actor_title_map = {}
-		actor_name_map = {}
-		for actor in actors:
-			cursor.execute(actor_titles_stmt.format(actor))
-			titles = []
-			names = []
-			for t in cursor:
-				print t
-				print "heyyyyyy"
-				titles.append(t[0])
-				names.append(t[1])
-			titles = titles[0].split(",")
-			actor_title_map[actor] = titles
-			names = names[0]
-			actor_name_map[actor] = names
-		print "Actor and Titles: \n" + str(actor_title_map)
-		print "Actor and Name: \n" + str(actor_name_map)
-
-		# get most popular actor according to titles count
-		max_actor_nconst = None
-		maxTitles = float("-inf")
-		for actor_nconst in actor_title_map:
-			titles = actor_title_map[actor_nconst]
-			print "Titles for {} : {}".format(actor_nconst,titles)
-			if len(titles) > maxTitles:
-				maxTitles = len(titles)
-				max_actor_nconst = actor_nconst
-		get_titles_stmt = "Select rating,primary_title,tconst from movie_info where tconst='{}'"
-		title_detailed_list = []
-		current_movie_rating = -1
-		current_movie_title = "BLANK"
-		for t in actor_title_map[max_actor_nconst]:
-			print "Getting rating for title {}".format(t)
-			cursor.execute(get_titles_stmt.format(t))
-			for m in cursor:
-				if m is not None:
-					rating = m[0]
-					title = m[1]
-					tconst = m[2]
-					movie_json = {}
-					movie_json["rating"] = rating
-					movie_json["title"] = title
-					if tconst == imdb_title_id:
-						current_movie_rating = rating
-						current_movie_title = title
-					title_detailed_list.append(movie_json)
-		print title_detailed_list
-		avg = getAverageRating(title_detailed_list)
-		percent_of_avg = current_movie_rating/avg
-		if percent_of_avg > 0.75:
-			comment = "Compared to other movies {} has acted in, users seem to love this movie! Average Rating: {}, {} : {}"
-		elif percent_of_avg > 0.5:
-			comment = "This movie is as good as the other movies {} has acted in. Average Rating: {}, {} : {}"
-		elif percent_of_avg > 0.25:
-			comment = "This movie is worse than other movies {} has acted in. Average Rating: {}, {} : {}"
-		else:
-			comment = "This movie is one of {}'s worst movies. Average Rating: {}, {} : {}"
-		cursor.close()
-		cnx.close()
-		return comment.format(actor_name_map[max_actor_nconst],avg,current_movie_title,current_movie_rating)
-	except Exception as e:
-		print e 
+# 		# get most popular actor according to titles count
+# 		max_actor_nconst = None
+# 		maxTitles = float("-inf")
+# 		for actor_nconst in actor_title_map:
+# 			titles = actor_title_map[actor_nconst]
+# 			print "Titles for {} : {}".format(actor_nconst,titles)
+# 			if len(titles) > maxTitles:
+# 				maxTitles = len(titles)
+# 				max_actor_nconst = actor_nconst
+# 		get_titles_stmt = "Select rating,primary_title,tconst from movie_info where tconst='{}'"
+# 		title_detailed_list = []
+# 		current_movie_rating = -1
+# 		current_movie_title = "BLANK"
+# 		for t in actor_title_map[max_actor_nconst]:
+# 			print "Getting rating for title {}".format(t)
+# 			cursor.execute(get_titles_stmt.format(t))
+# 			for m in cursor:
+# 				if m is not None:
+# 					rating = m[0]
+# 					title = m[1]
+# 					tconst = m[2]
+# 					movie_json = {}
+# 					movie_json["rating"] = rating
+# 					movie_json["title"] = title
+# 					if tconst == imdb_title_id:
+# 						current_movie_rating = rating
+# 						current_movie_title = title
+# 					title_detailed_list.append(movie_json)
+# 		print title_detailed_list
+# 		avg = getAverageRating(title_detailed_list)
+# 		percent_of_avg = current_movie_rating/avg
+# 		if percent_of_avg > 0.75:
+# 			comment = "Compared to other movies {} has acted in, users seem to love this movie! Average Rating: {}, {} : {}"
+# 		elif percent_of_avg > 0.5:
+# 			comment = "This movie is as good as the other movies {} has acted in. Average Rating: {}, {} : {}"
+# 		elif percent_of_avg > 0.25:
+# 			comment = "This movie is worse than other movies {} has acted in. Average Rating: {}, {} : {}"
+# 		else:
+# 			comment = "This movie is one of {}'s worst movies. Average Rating: {}, {} : {}"
+# 		cursor.close()
+# 		cnx.close()
+# 		return comment.format(actor_name_map[max_actor_nconst],avg,current_movie_title,current_movie_rating)
+# 	except Exception as e:
+# 		print e 
 
 @app.route('/')
 def index():
@@ -110,27 +101,140 @@ def get_movie_detail():
 	data = request.get_json(silent=True)
 	pp = pprint.PrettyPrinter(indent=4)
 	pp.pprint(data)
-	movie = data['queryResult']['parameters']['movie']
-	# api_key = OMDB_API_KEY
-
-	# movie_detail = requests.get('http://www.omdbapi.com/?t={0}&apikey={1}'.format(movie, api_key)).content
-	# movie_detail = json.loads(movie_detail)
-
-	# imdb_id = movie_detail['imdbID']
-	# print "IMDB = " + imdb_id
-	# final_statement = extract_actor_anomaly(imdb_id)
-	
-	# pp.pprint(movie_detail)
-	# response = """
-	# 	Title: {0}\n
-	# 	Released: {1}\n
-	# 	Actors: {2}\n
-	# 	Plot: {3}
-	# """.format(movie_detail['Title'], movie_detail['Released'], movie_detail['Actors'], movie_detail['Plot'])
-	# response = """ *Anomaly comment* : {7} *-----* '{0}' is a {5} language movie that was released in {6} on {1}. It was directed by '{3}', starring '{2}'
-	#  and the plot is as follows: '{4}'.""".format(movie_detail['Title'], movie_detail['Released'], movie_detail['Actors'], movie_detail['Director'],movie_detail['Plot'],movie_detail['Language'],movie_detail['Country'],final_statement)
-	reply = {
-		"fulfillmentText": tmdb.searchForMovie(movie),
-	}
-
+	if data['queryResult']['intent']['displayName'] == "MovieBasic":
+		print("aaa")
+		reply = movie_basic(data)
+		print("aaa")
+	elif data['queryResult']['intent']['displayName'] == "MovieRating":
+		print("bbb")
+		reply = movie_details(data,"vote")
+		print("bbb")
+	elif data['queryResult']['intent']['displayName'] == "MovieBudget":
+		print("ccc")
+		reply = movie_details(data,"budget")
+		print("ccc")
+	elif data['queryResult']['intent']['displayName'] == "MovieRevenue":
+		print("ddd")
+		reply = movie_details(data,"revenue")
+		print("ddd")
+	elif data['queryResult']['intent']['displayName'] == "MovieAnomaly":
+		print("eee")
+		reply = movie_anomaly(data)
 	return jsonify(reply)
+
+def movie_anomaly(data):
+	comment = "Sorry, could you try again with the movie name?"
+	if "outputContexts" in data['queryResult']:
+		output_contexts = data['queryResult']['outputContexts']
+		for context in output_contexts:
+			if context["name"].find("movie-actor-details") != -1:
+				stats = context["parameters"]["stats"]
+				if stats["votes"]["avg"] < context["parameters"]["vote"] :
+					comment = "You should definitely watch this movie! It has done better than most other movies {} has acted in! The rating for {} is {} while his average rating for the others is {}.".format(context["parameters"]["actor"],context["parameters"]["title"],context["parameters"]["vote"],stats["votes"]["avg"])
+				else:
+					comment = "Its an avoidable film. The movie has done worse than most other movies {} has acted in! The rating for {} is {} while his average rating for the others is {}.".format(context["parameters"]["actor"],context["parameters"]["title"],context["parameters"]["vote"],stats["votes"]["avg"])
+	reply = {
+		"fulfillmentText": comment,
+		"outputContexts": data['queryResult']['outputContexts'],
+		"payload": {
+			"google": {
+				"expectUserResponse": 'true',
+				"richResponse": {
+					"items": [
+						{
+							"simpleResponse": {
+								"textToSpeech": comment
+							}
+						}
+					]
+				}
+			}
+		}
+	}
+	return reply
+
+def movie_details(data,param):
+	rating = "Sorry, could you try again with the movie name?"
+	if "outputContexts" in data['queryResult']:
+		output_contexts = data['queryResult']['outputContexts']
+		for context in output_contexts:
+			if context["name"].find("movie-details") != -1:
+				rating = "The {} for the movie \"{}\" is {}".format(param, context["parameters"]["title"], context["parameters"][param])
+
+	reply = {
+		"fulfillmentText": rating,
+		"outputContexts": data['queryResult']['outputContexts'],
+		"payload": {
+			"google": {
+				"expectUserResponse": 'true',
+				"richResponse": {
+					"items": [
+						{
+							"simpleResponse": {
+								"textToSpeech": rating
+							}
+						}
+					]
+				}
+			}
+		}
+	}
+	return reply
+
+
+def movie_basic(data):
+	movie_name = data['queryResult']['parameters']['movie']
+	movie = tmdb.searchForMovie(movie_name)
+	crew = tmdb.getActorsAndDirectors(movie["id"])
+	top_movies = tmdb.getActorTopMovies(crew[0]["id"])
+	if "outputContexts" in data['queryResult']:
+		output_contexts = data['queryResult']['outputContexts']
+		for context in output_contexts:
+			if context["name"].find("movie-actor-details") != -1:
+				context["parameters"]["actor"] = crew[0]["name"]
+				context["parameters"]["stats"] = tmdb.getMoviesStats(top_movies)
+				context["parameters"]["title"] = movie["title"]
+				context["parameters"]["vote"] = movie["vote_average"]
+				context["parameters"]["id"] = movie["id"]
+				context["parameters"]["budget"] = movie["budget"]
+				context["parameters"]["revenue"] = movie["revenue"]
+			if context["name"].find("movie-details") != -1:
+				context["parameters"]["title"] = movie["title"]
+				context["parameters"]["vote"] = movie["vote_average"]
+				context["parameters"]["id"] = movie["id"]
+				context["parameters"]["budget"] = movie["budget"]
+				context["parameters"]["revenue"] = movie["revenue"]
+	reply = {
+		"fulfillmentText": "'Release Date': {},\n 'Overview': {}".format(movie["release_date"], movie["overview"]),
+		# "fulfillmentMessages" : [
+		# 	{
+		# 		"text" :  {
+		# 			"text" : [ 
+		# 			"'Release Date': {},\n 'Overview': {}".format(movie["release_date"], movie["overview"]), "Liked that?"
+		# 			]
+		# 		}
+		# 	}
+		# ],
+		"outputContexts": data['queryResult']['outputContexts'],
+		"payload": {
+			"google": {
+				"expectUserResponse": 'true',
+				"richResponse": {
+					"items": [
+						{
+							"simpleResponse": {
+								"textToSpeech": "'Release Date': {},\n 'Overview': {}".format(movie["release_date"], movie["overview"])
+							}
+						},
+						{
+							"simpleResponse": {
+								"textToSpeech": "\n\nWant more information?"
+							}
+						}
+					]
+				}
+			}
+		}
+	}
+	pprint.pprint(reply)
+	return reply
--- movie_app/tmdb.py
+++ movie_app/tmdb.py
@@ -1,9 +1,13 @@
 import requests
 import pprint
+import numpy as np
 
 TMDB_API_KEY = "b468692645623d8d1a926f516015f1c1"
 SEARCH_MOVIE_URL = "https://api.themoviedb.org/3/search/movie"
 CREW_URL = "https://api.themoviedb.org/3/movie/{}/credits"
+PERSON_MOVIE_CREDITS_URL = "https://api.themoviedb.org/3/person/{}/movie_credits"
+GET_MOVIE_BY_ID_URL = "https://api.themoviedb.org/3/movie/{}"
+
 
 def getParams():
     params = {
@@ -12,31 +16,135 @@ def getParams():
     }
     return params
 
+# def compareCurrentMovieWithOthers(current_movie, overall_stats):
+
+
+'''
+{
+    'adult': False,
+    'backdrop_path': '/icmmSD4vTTDKOq2vvdulafOGw93.jpg',
+    'genre_ids': [28, 878],
+    'id': 603,
+    'original_language': 'en',
+    'original_title': 'The Matrix',
+    'overview': 'Set in the 22nd century, The Matrix tells the story of a '
+                'computer hacker who joins a group of underground insurgents '
+                'fighting the vast and powerful computers who now rule the earth.',
+    'popularity': 31.893,
+    'poster_path': '/hEpWvX6Bp79eLxY1kX5ZZJcme5U.jpg',
+    'release_date': '1999-03-30',
+    'title': 'The Matrix',
+    'video': False,
+    'vote_average': 8.1,
+    'vote_count': 13548
+}
+'''
 def searchForMovie(movName):
     queryParams = getParams()
     queryParams['query'] = movName
     response = requests.request("GET", SEARCH_MOVIE_URL, params=queryParams).json()
     movie = response["results"][0]
-    title = movie['original_title']
-    plot = movie['overview']
-    release_date = movie['release_date']
-    rating = movie['vote_average']
-    movie_id = movie['id']
-    actors,dirs = getActorsAndDirectors(movie_id)
-    result = "{} : '{}', 'Release Date' : {} , 'Directors': {}, 'Actors': {}, 'Rating': {}".format(title,plot,release_date,dirs,actors,rating)
-    print "hiii\n" + result
-    return result
+    response2 = requests.request("GET", GET_MOVIE_BY_ID_URL.format(movie["id"]), params=queryParams).json()
+    movie["budget"] = response2["budget"]
+    movie["revenue"] = response2["revenue"]
+    return movie
 
 
+'''
+{  
+    'cast_id': 34,
+    'character': 'Thomas A. Anderson / Neo',
+    'credit_id': '52fe425bc3a36847f80181c1',
+    'gender': 2,
+    'id': 6384,
+    'name': 'Keanu Reeves',
+    'order': 0,
+    'profile_path': '/bOlYWhVuOiU6azC4Bw6zlXZ5QTC.jpg'
+}
+'''
 def getActorsAndDirectors(movieId):
     queryParams = getParams()
     response = requests.request("GET", CREW_URL.format(movieId), params=queryParams).json()
     cast = response['cast']
-    actors = "{} {} {}".format(cast[0]['name'], cast[1]['name'], cast[2]['name'])
-    crew = response['crew']
-    directors = [c['name'] for c in crew if c['job'] == 'Director']
-    dirs = ""
-    for d in directors:
-        dirs += d + " "
-    return actors,dirs
-    
+    return cast
+
+'''
+[{movie1},{movie2}...]
+'''
+def getActorTopMovies(actorId):
+    queryParams = getParams()
+    # print("wogooooo")
+    response = requests.request("GET", PERSON_MOVIE_CREDITS_URL.format(actorId), params=queryParams).json()
+    cast = response["cast"]
+    id_popularity_map = {}
+    for movie in cast:
+        id_popularity_map[movie['id']] = movie['popularity']
+    from collections import OrderedDict
+    sorted_popularity_dict = OrderedDict(sorted(id_popularity_map.items(), key=lambda x: x[1], reverse=True))
+    movies_data_list = []
+    # print("damn")
+    for idx,movie_id in enumerate(sorted_popularity_dict):
+        queryParams = getParams()
+        response = requests.request("GET", GET_MOVIE_BY_ID_URL.format(movie_id), params=queryParams).json()
+        movies_data_list.append(response)
+        # print("okay {}".format(idx))
+        if(idx == 5):
+            break
+    return movies_data_list
+
+'''
+{
+    "votes" : {
+        "avg" : x,
+        "min" : x,
+        "max" : x
+    },
+    "revenue" : {
+        "avg" : x,
+        "min" : x,
+        "max" : x
+    },
+    "budget" : {
+        "avg" : x,
+        "min" : x,
+        "max" : x
+    }
+}
+'''
+def getMoviesStats(movie_list):
+    votes, revenue, budget = [],[],[] 
+    for movie in movie_list:
+        if movie["vote_count"] < 50:
+            pass
+        if movie["vote_average"] != 0:
+            votes.append(movie["vote_average"])
+        if movie["revenue"] != 0:
+            revenue.append(movie["revenue"])
+        if movie["budget"] != 0:
+            budget.append(movie["budget"])
+    votes = np.array(votes)
+    revenue = np.array(revenue)
+    budget = np.array(budget)
+    avg_vote = np.mean(votes).item()
+    avg_revenue = np.mean(revenue).item()
+    avg_budget = np.mean(budget).item()
+    max_vote, max_revenue, max_budget = np.max(votes).item(), np.max(revenue).item(), np.max(budget).item()
+    min_vote, min_revenue, min_budget = np.min(votes).item(), np.min(revenue).item(), np.min(budget).item()
+    result = {
+        "votes" : {
+            "avg" : avg_vote,
+            "min" : min_vote,
+            "max" : max_vote
+        },
+        "revenue" : {
+            "avg" : avg_revenue,
+            "min" : min_revenue,
+            "max" : max_revenue
+        },
+        "budget" : {
+            "avg" : avg_budget,
+            "min" : min_budget,
+            "max" : max_budget
+        }
+    }
+    return result
